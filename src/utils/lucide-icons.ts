8086f8ab05563a2b08ed6cd722241acdf38e5639
chaerla <chelgchen@gmail.com>
2024-03-31 00:52:22 +0700

    feat: add lucide icons

---
+++ src/utils/lucide-icons.ts
@@ -0,0 +1,18 @@
+import {
+  createIcons,
+  Square,
+  RectangleHorizontal,
+  Slash,
+  Pentagon
+} from 'lucide';
+
+export const initializeIcons = () => {
+  createIcons({
+    icons: {
+      Square,
+      RectangleHorizontal,
+      Slash,
+      Pentagon
+    }
+  });
+};
