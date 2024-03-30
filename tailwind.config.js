2029a2a1b2646b8a7f90e16d60671100fbf03ba7
chaerla <chelgchen@gmail.com>
2024-03-31 00:52:29 +0700

    feat: updated styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: 'var(--tp-base-background-color)',
+        secondary: 'var(--tp-container-background-color)',
+        tertiary: 'var(--tp-monitor-background-color)',
+        'primary-fg': 'var(--tp-container-foreground-color)'
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 };
